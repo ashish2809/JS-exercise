62ac8471d833513223d956b88d5b2f5a2f1c30bb
Ashish <ashish.srivastava1@tothenew.com>
2021-09-16 00:26:15 +0530

    modified

--- area.js
+++ area.js
@@ -1,13 +1,3 @@
-// function circle(radius) {
-//     this.radius = radius;
-    
-//     this.area = function () {
-//         return Math.PI * this.radius * this.radius;
-//     };
-// }
-// let c = prompt("enter the radius of circle: ")
-// let circ = new circle(c);
-// prompt('Area of circle =', +circ.area().toFixed(2));
 
 
 function circle(radius){
